01504d94decdd5f01a7763edfcc742aa6182d0a5
Nicole Lacerda <nicole.lacerda05@gmail.com>
2024-03-30 23:21:06 -0300

    Atualizações páginas

--- app.py
+++ app.py
@@ -1,9 +1,20 @@
-  ```python
-  from flask import Flask
+from flask import Flask, render_template, request, redirect, url_for
+app = Flask(__name__) 
 
-  app = Flask(__name__)
 
-  @app.route("/")
-  def index():
-      return "Olá, <b>tudo bem</b>?"
-  ```
+@app.route("/apresentacao")
+def apresentacao():
+      return render_template("apresentacao.html")
+
+
+@app.route("/materias")
+def materias():
+      return render_template("materias.html")
+
+
+@app.route("/contato")
+def contato():
+      return render_template("contato.html")
+ 
+if __name__ == '__main__':
+  app.run(port=5000, debug=True)
