c11a3373f7706d0180b870bbb31fb07e8fe73d9f
Nicole Lacerda <nicole.lacerda05@gmail.com>
2024-03-30 23:40:13 -0300

    Atualizacao paginas

--- app.py
+++ app.py
@@ -4,17 +4,17 @@ app = Flask(__name__)
 
 @app.route("/apresentacao")
 def apresentacao():
-      return render_template("apresentacao.html")
+    return render_template("apresentacao.html")
 
 
 @app.route("/materias")
 def materias():
-      return render_template("materias.html")
+    return render_template("materias.html")
 
 
 @app.route("/contato")
 def contato():
-      return render_template("contato.html")
+    return render_template("contato.html")
  
 if __name__ == '__main__':
   app.run(port=5000, debug=True)
