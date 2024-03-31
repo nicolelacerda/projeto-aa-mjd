cb87565f8d622bad3cc5df7234ee8285242ae15f
Nicole Lacerda <nicole.lacerda05@gmail.com>
2024-03-31 00:16:59 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,15 @@
-from flask import Flask
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-@app.route("/")
-def index():
-    return "Olá, <b>tudo bem</b>?"
-  
+@app.route("/apresentacao")
+def apresentacao():
+    return render_template("apresentacao.html")
+
+@app.route("/materias")
+def materias():
+    return render_template("materias.html")
+
+@app.route("/contato")
+def contato():
+    return render_template("contato.html")
