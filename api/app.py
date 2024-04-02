fe8df3dd86f6ccc3cb34782eeedb6ec7b3e3d6cc
Nicole Lacerda <nicole.lacerda05@gmail.com>
2024-04-02 19:53:48 -0300

    update

--- api/app.py
+++ api/app.py
@@ -1,3 +1,6 @@
+import requests
+from bs4 import BeautifulSoup
+import pandas as pd
 from flask import Flask, render_template, request
 
 app = Flask(__name__)
@@ -8,18 +11,34 @@ def apresentacao():
 
 @app.route("/materias")
 def materias():
-    return render_template("materias.html")
+    req_jota_melissa=requests.get("https://www.jota.info/autor/melissa-duartejota-info")
+    conteudo_jota_melissa=req_jota_melissa.content
+    html_jota_melissa=BeautifulSoup(conteudo_jota_melissa)
+    noticias=html_jota_melissa.findAll('div',{'class':'jota-posts-list__body'})
+
+    materias_melissa=[]
+    for noticia in noticias:
+        titulo=noticia.find('h3').text.strip()
+        link=noticia.find('a').get('href')
+        materias_melissa.append({"titulo": titulo, "link": link})
+    print(materias_melissa)    
+    return render_template("materias.html" , materias=materias_melissa)
 
 @app.route("/contato")
 def contato():
     enviado = False
+    return render_template("contato.html")
+
+@app.route("/enviar_contato" , methods=["POST" , "GET"])
+def enviar_contato():
     if request.method == "POST":
         nome = request.form["nome"]
         email = request.form["email"]
         mensagem = request.form["mensagem"]
         enviado = True
-        return render_template("contato.html", enviado=enviado, nome=nome, mensagem=mensagem)
-        return render_template("contato.html", enviado=enviado)
+        print(mensagem)
+        return "Deu certo"
+
 
 
 if __name__ == '__main__':
